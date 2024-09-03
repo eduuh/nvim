34e34421021b6879904be1310588f392ff808f70
eduuh <31909722+eduuh@users.noreply.github.com>
2024-09-03 19:44:54 +0000

    Fix mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,73 +1,78 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
+	{
 		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
-
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-
-		local mason_tool_installer = require("mason-tool-installer")
-
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+		config = function()
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"tsserver",
+					"html",
+					"rust-analyzer",
+					"cssls",
+					"tailwindcss",
+					"svelte",
+					"lua_ls",
+					"graphql",
+					"emmet_ls",
+					"prismals",
+					"pyright",
+					"lua_ls",
+					"cmake",
+					"dockerls",
+					"jsonls",
+					"clangd",
+					"jdtls",
 				},
-			},
-		})
+				auto_update = false,
+				run_on_start = true,
+				start_delay = 0,
+				debounce_hours = 5,
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			-- import mason
+			local mason = require("mason")
 
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"pyright",
-				"lua_ls",
-				"cmake",
-				"dockerls",
-				"jsonls",
-				"clangd",
-				"jdtls",
-			},
-			auto_update = false,
-			run_on_start = true,
-			start_delay = 0,
-			debounce_hours = 5,
-		})
+			-- import mason-lspconfig
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint",
-				"eslint_d",
-				"vim-language-server",
-				"prettier",
-				"prettierd",
-				"clang-format",
-				"js-debug-adapter",
-				"stylua",
-				"chrome-debug-adapter",
-				"codelldb",
-				"cpptools",
-				"rust-analyzer",
-			},
-		})
-	end,
+			local mason_tool_installer = require("mason-tool-installer")
+
+			-- enable mason and configure icons
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"prettier", -- prettier formatter
+					"stylua", -- lua formatter
+					"isort", -- python formatter
+					"black", -- python formatter
+					"pylint",
+					"eslint_d",
+					"vim-language-server",
+					"prettier",
+					"prettierd",
+					"clang-format",
+					"js-debug-adapter",
+					"stylua",
+					"chrome-debug-adapter",
+					"codelldb",
+					"cpptools",
+				},
+			})
+		end,
+	},
 }
