2db0c910563fb83a63510b6e7239e6545c83f253
eduuh <eduuh.muraya@outlook.com>
2024-09-08 12:31:45 +0300

    Add more text objects

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -1,97 +1,106 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		vim.filetype.add({ extension = { mdx = "mdx" } })
-		vim.treesitter.language.register("markdown", "mdx")
+	{
+		"chrisgrieser/nvim-various-textobjs",
+		lazy = true,
+		keys = {
+			-- ...
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			vim.filetype.add({ extension = { mdx = "mdx" } })
+			vim.treesitter.language.register("markdown", "mdx")
 
-		require("nvim-treesitter.configs").setup({
-			-- Add languages to be installed here that you want installed for treesitter
-			ensure_installed = {
-				"cpp",
-				"c",
-				"lua",
-				"rust",
-				"typescript",
-				"regex",
-				"bash",
-				"markdown",
-				"markdown_inline",
-				"kdl",
-				"sql",
-				"org",
-				"html",
-				"css",
-				"javascript",
-				"yaml",
-				"json",
-				"toml",
-				"lua",
-				"xml",
-				"json",
-				"graphql",
-			},
-
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<c-space>",
-					node_incremental = "<c-space>",
-					scope_incremental = "<c-s>",
-					node_decremental = "<c-backspace>",
+			require("nvim-treesitter.configs").setup({
+				-- Add languages to be installed here that you want installed for treesitter
+				ensure_installed = {
+					"cpp",
+					"c",
+					"lua",
+					"rust",
+					"typescript",
+					"regex",
+					"bash",
+					"markdown",
+					"markdown_inline",
+					"kdl",
+					"sql",
+					"org",
+					"html",
+					"css",
+					"javascript",
+					"yaml",
+					"json",
+					"toml",
+					"lua",
+					"xml",
+					"json",
+					"graphql",
 				},
-			},
-			textobjects = {
-				select = {
+
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
 					enable = true,
-					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["aa"] = "@parameter.outer",
-						["ia"] = "@parameter.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-						["ii"] = "@conditional.inner",
-						["ai"] = "@conditional.outer",
-						["il"] = "@loop.inner",
-						["al"] = "@loop.outer",
-						["at"] = "@comment.outer",
+						init_selection = "<c-space>",
+						node_incremental = "<c-space>",
+						scope_incremental = "<c-s>",
+						node_decremental = "<c-backspace>",
 					},
 				},
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["ii"] = "@conditional.inner",
+							["ai"] = "@conditional.outer",
+							["il"] = "@loop.inner",
+							["al"] = "@loop.outer",
+							["at"] = "@comment.outer",
+						},
 					},
-					goto_next_end = {
-						["]F"] = "@function.outer",
-						["]C"] = "@class.outer",
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]C"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[c"] = "@class.outer",
+						},
 					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[c"] = "@class.outer",
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
 					},
 				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
-				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
