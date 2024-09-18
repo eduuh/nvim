5d8a1a5210feade2671a63adfa446404cb0c95c1
Edwin Muraya <edwinmuraya@microsoft.com>
2024-09-18 07:26:29 +0000

    stable: move toggle term command to toggle plugin config

--- lua/plugins/vimtest.lua
+++ lua/plugins/vimtest.lua
@@ -1,3 +1,17 @@
+vim.cmd([[
+  let test#strategy = {
+  \ 'nearest': 'neovim',
+  \ 'file':    'dispatch',
+  \ 'suite':   'basic',
+\}
+
+  nmap <silent> <leader>tt :TestNearest<CR>
+  nmap <silent> <leader>tT :TestFile<CR>
+  nmap <silent> <leader>ta :TestSuite<CR>
+  nmap <silent> <leader>tl :TestLast<CR>
+  nmap <silent> <leader>tv :TestVisit<CR>
+]])
+
 return {
 	"vim-test/vim-test",
 	"tpope/vim-dispatch",
