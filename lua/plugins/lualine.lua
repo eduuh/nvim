91fc17ee27aa12d0ed38b2efc7598108be536273
eduuh <31909722+eduuh@users.noreply.github.com>
2024-12-28 00:04:29 +0300

    Update: Add rest plugins

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,5 +2,22 @@ return {
 	"nvim-lualine/lualine.nvim",
 	lazy = false, -- load immediately so there is no flickering
 	dependencies = "nvim-tree/nvim-web-devicons",
-	config = true,
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					{
+						"rest",
+						icon = "",
+						fg = "#428890",
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
