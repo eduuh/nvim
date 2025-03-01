bcc8d81cb71a7e4a56aa8771b9996477e3246316
eduuh <31909722+eduuh@users.noreply.github.com>
2025-03-01 07:48:16 +0300

    Remove unneeded stuff

--- init.lua
+++ init.lua
@@ -1,12 +1,5 @@
-require("config.osconfigs")
 require("config.options")
 require("config.lazy")
 require("config.autocmd")
 require("config.keymapping")
 require("config.bash")
-
-vim.filetype.add({
-	extension = {
-		["http"] = "http",
-	},
-})
