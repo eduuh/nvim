5f804b7c0a842af2e6e7a1a5ee7152835624fff1
eduuh <31909722+eduuh@users.noreply.github.com>
2025-04-16 12:44:33 +0300

    [Ai] Add Avante & MCP hub plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,42 +1,5 @@
 return {
 	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		lazy = false,
-		enabled = true,
-		branch = "main",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			agent = "copilot",
-			debug = false, -- Enable debugging
-			model = "claude-3.5-sonnet",
-			question_header = "## User ", -- Header to use for user questions
-			answer_header = "## Copilot ", -- Header to use for AI answers
-			error_header = "## Error ", -- Header to use for errors
-		},
-		keys = {
-			{ "<leader>c", ":CopilotChatToggle<CR>", desc = "Copilot toggle" },
-			{ "<leader>cc", ":CopilotChatCommitStaged<CR>", desc = "Copilot create staged" },
-			{ "<leader>co", ":CopilotChatOptimize<CR>", desc = "Copilot create staged" },
-			{ "<leader>cr", ":CopilotChatReview<CR>", desc = "Copilot create staged" },
-			{ "<leader>ce", ":CopilotChatExplain<CR>", desc = "Copilot create staged" },
-			{
-				"<leader>cq",
-				function()
-					local input = vim.fn.input("Quick Chat: ")
-					if input ~= "" then
-						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
-					end
-				end,
-				desc = "CopilotChat - Quick chat selected",
-				mode = { "v" },
-			},
-		},
-	},
-	{
 		"zbirenbaum/copilot.lua",
 		enabled = true,
 		cmd = "Copilot",
@@ -86,6 +49,74 @@ return {
 				server_opts_overrides = {},
 			})
 		end,
+	},
+	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+		},
+		-- comment the following line to ensure hub will be ready at the earliest
+		cmd = "MCPHub", -- lazy load by default
+		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+		config = function()
+			require("mcphub").setup()
+		end,
+	},
+	{
+		"yetone/avante.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"zbirenbaum/copilot.lua",
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = { file_types = { "markdown", "Avante" } },
+				ft = { "markdown", "Avante" },
+			},
+		},
+		build = "make",
+		opts = { provider = "copilot" },
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		lazy = false,
+		enabled = true,
+		branch = "main",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			agent = "copilot",
+			debug = false, -- Enable debugging
+			model = "claude-3.5-sonnet",
+			question_header = "## User ", -- Header to use for user questions
+			answer_header = "## Copilot ", -- Header to use for AI answers
+			error_header = "## Error ", -- Header to use for errors
+		},
+		keys = {
+			{ "<leader>c", ":CopilotChatToggle<CR>", desc = "Copilot toggle" },
+			{ "<leader>cc", ":CopilotChatCommitStaged<CR>", desc = "Copilot create staged" },
+			{ "<leader>co", ":CopilotChatOptimize<CR>", desc = "Copilot create staged" },
+			{ "<leader>cr", ":CopilotChatReview<CR>", desc = "Copilot create staged" },
+			{ "<leader>ce", ":CopilotChatExplain<CR>", desc = "Copilot create staged" },
+			{
+				"<leader>cq",
+				function()
+					local input = vim.fn.input("Quick Chat: ")
+					if input ~= "" then
+						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
+					end
+				end,
+				desc = "CopilotChat - Quick chat selected",
+				mode = { "v" },
+			},
+		},
 	},
 	{
 		"jackMort/ChatGPT.nvim",
